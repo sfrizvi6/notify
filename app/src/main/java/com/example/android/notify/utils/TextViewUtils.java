e71fdf0826d32ac4cb9fbcd2c5a57d43ebf51474
Fatima Rizvi <fsyeda@linkedin.com>
2018-03-21 20:21:50 -0700

    TextViewUtils.setTextAndVisibility

--- app/src/main/java/com/example/android/notify/utils/TextViewUtils.java
+++ app/src/main/java/com/example/android/notify/utils/TextViewUtils.java
@@ -1,8 +1,16 @@
 package com.example.android.notify.utils;
 
-/**
- * Created by fsyeda on 3/20/18.
- */
+import android.view.View;
+import android.widget.TextView;
 
 public class TextViewUtils {
+
+    public static void setTextAndVisibility(TextView textView, CharSequence text) {
+        if (text == null || text.length() < 1) {
+            textView.setVisibility(View.GONE);
+        } else {
+            textView.setText(text);
+            textView.setVisibility(View.VISIBLE);
+        }
+    }
 }
